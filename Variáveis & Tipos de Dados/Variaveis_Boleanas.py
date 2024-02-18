33a559fcc5f981b17de9243f84c1093ea02101a5
Miqueias Lopes <miqueiaslopess93@gmail.com>
2024-02-18 09:26:30 -0300

    chore: Adicionando pasta Variáveis & Tipos de Dados

---
+++ Variáveis & Tipos de Dados/Variaveis_Boleanas.py
@@ -0,0 +1,20 @@
+# ---------- TELA USUARIO ----------#
+usuario_cadastro = input("Usuario: ")
+senha_cadastro = str(input("Senha: "))
+
+# ----------BANCO DE DADOS ----------#
+usuario_email = "lucianalopes@gmail.com"
+senha = "te amo miqueias"
+
+# VALIDANDO USUARIO E SENHA
+usuario_igual = usuario_cadastro == usuario_email
+senha_igual = senha == senha_cadastro
+
+# VALIDANDO ACESSO
+conceder_acesso = usuario_igual & senha_igual
+
+if (conceder_acesso):
+    print("Login Permitido")
+
+else:
+    print("Login Negado")
