81d029e3cdc51b33275c8fd67e7c2e3d7f9e7ef9
Miqueias Lopes <miqueiaslopess93@gmail.com>
2024-02-18 09:41:18 -0300

    Renomeando nome da pasta Variáveis_&_Tipos _de_Dados

---
+++ Variáveis_&_Tipos _de_Dados/Variaveis.py
@@ -0,0 +1,24 @@
+#idade = 30
+#print(idade)
+
+
+preco = 1000
+tipo_preco = type(preco)
+
+print (preco)
+print (tipo_preco)
+
+
+juros = 0.5
+tipo_juros = type(juros)
+
+print (juros)
+print (tipo_juros)
+
+
+telefone_fixo = None
+
+print(telefone_fixo)
+print(type(telefone_fixo))
+
+print (type(37))
